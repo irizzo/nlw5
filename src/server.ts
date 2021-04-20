fc7d00c5b00a8658f6121e4d08699c7c2177320b
irizzo <isabelle965rizzo@gmail.com>
2021-04-20 09:17:08 -0300

    starting the project

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+const app = express();
+
+const port = 3333
+
+app.get('/', (req, res) => {
+  return res.json({
+    message: 'oi'
+  })
+})
+
+app.post('/', (req, res) => {
+  return res.json({
+    message: 'Usuário salvo com sucesso'
+  })
+})
+
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
